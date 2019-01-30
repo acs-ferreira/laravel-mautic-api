aa1a6b5d9b74324477b4a295bae2a4208961675a
Carlos Ferreira <c.ferreira@acsiiweb.com>
2019-01-30 18:15:26 +0100

    Update MauticException.php

--- src/Exceptions/MauticException.php
+++ src/Exceptions/MauticException.php
@@ -1,10 +1,12 @@
-<?php namespace Princealikhan\Mautic\Exceptions;
+<?php
+
+namespace Princealikhan\Mautic\Exceptions;
+
 /**
  * Invalid Request Exception
  *
  * Thrown when a request is invalid or missing required fields.
  */
 class MauticException extends \Exception
-{
-	
+{	
 }
